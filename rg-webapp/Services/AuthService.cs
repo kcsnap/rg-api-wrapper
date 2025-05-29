10180c39a0e621e11644a55f46ceaca183e79000
kceden <Kenneth.COX@edenred.com>
2025-05-29 15:41:15 +0100

    added test website and auth

---
+++ rg-webapp/Services/AuthService.cs
@@ -0,0 +1,22 @@
+﻿using rg_wellbeing.Auth;
+using rg_wellbeing.Auth.Models;
+
+namespace RgWebapp.Services
+{
+    public class AuthService
+    {
+        Authentication authentication;
+
+        public async Task Authenticate(string clientId, string clientSecret, string partnerId)
+        {
+            authentication = new Authentication(new RgPartnerCredentials()
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret,
+                PartnerId = partnerId
+            });
+
+            var token = await authentication.GetAccessTokenAsync();
+        }
+    }
+}
